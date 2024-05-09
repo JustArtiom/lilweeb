263dd633b0b34470945f1464def22e950ff9bf65
JustArtiom <artiomsiatat@gmail.com>
2024-05-10 00:51:56 +0100

    training and use

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-# Training script v1.0.0
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import logging
+logging.getLogger('tensorflow').setLevel(logging.ERROR)
+
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import tools
+import os
 
 train_file = 'conversation.txt'
 oov_token = '</OOV/>'
@@ -20,19 +25,24 @@ tokenizer = tools.create_tokenizer(training_data, oov_token)
 
 model = load_model("test.h5")
 
-seed_text = "hello " + separator_token
-next_words = 300
+chat=""
 
-for _ in range(next_words):
-    token_list = tokenizer.texts_to_sequences([seed_text])[0]
-    token_list = pad_sequences(
-        [token_list], maxlen=max_tokens)
-    predicted = np.argmax(model.predict(token_list), axis=-1)
-    output_word = ""
-    for word, index in tokenizer.word_index.items():
-        if index == predicted:
-            output_word = word
-            break
-    seed_text += " " + output_word
+while True:
+    print()
+    chat += " " + input("> ") + " " + separator_token 
 
-print(seed_text.replace(separator_token+" ", "\n"))
+    response = ""
+    print(response, end='', flush=True)
+    while not response.endswith(separator_token):
+        token_list = tokenizer.texts_to_sequences([chat])[0]
+        token_list = pad_sequences(
+            [token_list], maxlen=max_tokens)
+        predicted = np.argmax(model.predict(token_list, verbose=0), axis=-1)
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        chat += " " + output_word
+        response += " " + output_word
+        print('\r' + response.replace(separator_token, ""), end='', flush=True)
--- train.py
+++ train.py
@@ -1,17 +1,17 @@
 # Training script v1.0.0
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Embedding
+from keras.layers import LSTM, Dense, Embedding, Dropout
 from keras.preprocessing.sequence import pad_sequences
 import tools
 
 train_file = 'conversation.txt'
 oov_token = '</OOV/>'
-separator_token = '69un1qw0rd'  # A unique word to use as a separator
+separator_token = '69un1qw0rd'
 max_tokens = 128
 
 save_path = "./models"
-epochs = 25
+epochs = 200
 batch_size = 512
 
 training_data = tools.read_train_data(train_file)
@@ -27,12 +27,14 @@ y = tools.one_hot_encoded(y, total_uniq_words)
 
 model = Sequential()
 model.add(Embedding(total_uniq_words, output_dim=128, input_length=max_tokens))
-model.add(LSTM(150))
+model.add(LSTM(256, return_sequences=True)) 
+model.add(Dropout(0.2)) 
+model.add(LSTM(256)) 
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(total_uniq_words, activation='softmax'))
-print(model.summary())
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adam', metrics=['accuracy'])
-
 model.fit(X, y, epochs=epochs, batch_size=batch_size, verbose=1)
 model.save("test.h5")
